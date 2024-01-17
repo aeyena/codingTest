fe832210e664fd94c9671c879427eb96911892cf
aeyena <121078071+aeyena@users.noreply.github.com>
2024-01-17 21:00:18 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+def solution(brown, yellow):
+    total = brown + yellow #총 카펫 수 
+    for i in range(1, int((total+1)/2)): #효율을 위해 절반만 
+        if(total%i==0): 
+            pairOne = int(total/i) #i의 짝꿍 구하기(약수)
+            if((i+pairOne)*2 - 4 == brown): #테두리 카펫수로 답 구하기
+                return [pairOne,i]
