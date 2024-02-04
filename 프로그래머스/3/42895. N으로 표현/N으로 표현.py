79b26983d89314f88a0775ffbb7d200ab37bdaf2
aeyena <121078071+aeyena@users.noreply.github.com>
2024-02-04 12:56:59 +0900

    [level 3] Title: N으로 표현, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,30 @@
+def calculate_n(X,Y):
+    n_set = set()
+    for x in X:
+        for y in Y:
+            n_set.add(x+y)
+            n_set.add(x-y)
+            n_set.add(x*y)
+            if y != 0:
+                n_set.add(x/y)
+    return n_set
+
+def solution(N, number):
+    answer = -1
+    result = {} # key는 숫자 사용횟수, value는 숫자를 key번 사용했을 때 나오는 연산 결과set
+    result[1] = {N} # N을 한 번 사용했을 때는 연산결과로 N만 나오므로
+    if number == N:
+        return 1
+    for n in range(2,9): # 8번까지만 계산
+        i = 1
+        temp_set = {int(str(N)*n)} # N=5, 2번 사용할 때 먼저 55를 저장
+        # 1(op)N-1 ... n-1(op)1 까지 계산
+        while i < n:
+            temp_set.update(calculate_n(result[i], result[n-i]))
+            i += 1
+        # 만들어진 set에 number와 같은 숫자가 있으면 stop
+        if number in temp_set:
+            answer = n
+            break
+        result[n] = temp_set
+    return answer
