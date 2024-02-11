dfe7ac4fda602818d17b69fb9eeb64fc67b742ab
aeyena <121078071+aeyena@users.noreply.github.com>
2024-02-11 16:16:53 +0900

    저자 별 카테고리 별 매출액 집계하기.sql

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,10 +1,17 @@
-select b.author_id, a.author_name, b.category, sum(b.price*bs.count) total_sales
-from book b inner join (
---도서 별 총 매출량
-select book_id, sum(sales) count
-from book_sales
-where '202201' = to_char(sales_date,'YYYYMM')
-group by book_id) bs on (b.book_id=bs.book_id)
-inner join author a on (b.author_id=a.author_id)
-group by b.category, b.author_id, a.author_name
-order by b.author_id asc, b.category desc
+select 
+  b.author_id, 
+  a.author_name, 
+  b.category, 
+  sum(b.price*bs.count) total_sales --총 매출액
+from 
+  book b inner join (
+    --도서 별 총 매출량
+    select book_id, sum(sales) count
+    from book_sales
+    where '202201' = to_char(sales_date,'YYYYMM')
+    group by book_id) bs on (b.book_id=bs.book_id)
+  inner join author a on (b.author_id=a.author_id)
+group by  
+  b.category, b.author_id, a.author_name
+order by 
+  b.author_id asc, b.category desc
