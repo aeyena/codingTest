144dbb07fc75e33e2d5785e7b795bc944dd3becc
aeyena <121078071+aeyena@users.noreply.github.com>
2024-03-07 22:35:44 +0900

    Update 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,11 +1,16 @@
-select to_number(to_char(start_date,'MM')) month, car_id, count(history_id) records
+select to_number(to_char(start_date,'MM')) month, 
+       car_id, 
+       count(history_id) records
 from CAR_RENTAL_COMPANY_RENTAL_HISTORY
 where car_id in 
-(select car_id
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-where start_date between to_date('20220801','YYYY-MM-DD') and to_date('20221031','YYYY-MM-DD')
-having count(car_id) >= 5
-group by car_id)
+            (
+             select car_id
+             from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+             where start_date between to_date('20220801','YYYY-MM-DD') 
+             and to_date('20221031','YYYY-MM-DD')
+             having count(car_id) >= 5
+             group by car_id
+            )
 and start_date between to_date('20220801','YYYY-MM-DD') and to_date('20221031','YYYY-MM-DD')
 group by to_char(start_date,'MM'), car_id
 order by to_char(start_date,'MM'), car_id desc
